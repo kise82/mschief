675bc2d2f00e142703ca2e0ab9736a91c59bbfe0
kise82 <kise82@pm.me>
2026-09-06 22:07:09 +0000

    `mschief`: Make `lexer_test` accept an optional file argument
    Signed-off-by: kise82 <kise82@pm.me>

--- src/bin/lexer_test.rs
+++ src/bin/lexer_test.rs
@@ -1,20 +1,39 @@
 use mschief::lexer::Lexer;
 
-use std::io::{self, BufRead};
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, Read},
+    process,
+};
 
 fn main() {
-    let mut buffer = String::with_capacity(1_024);
-    let mut handle = io::stdin().lock();
+    let file = env::args().nth(1);
+    let mut buffer = String::with_capacity(8_192);
 
-    while let Ok(n) = handle.read_line(&mut buffer)
-        && n > 0
-    {
+    if let Some(file) = file {
+        let Ok(mut file) = File::open(file) else {
+            process::exit(1);
+        };
+        let Ok(_) = file.read_to_string(&mut buffer) else {
+            process::exit(1);
+        };
+
         let lexer = Lexer::new(&buffer);
-
         for token in lexer {
             print!("{token:?} ");
         }
+    } else {
+        let mut handle = io::stdin().lock();
 
-        buffer.clear();
+        while let Ok(n) = handle.read_line(&mut buffer)
+            && n > 0
+        {
+            let lexer = Lexer::new(&buffer);
+            for token in lexer {
+                print!("{token:?} ");
+            }
+            buffer.clear();
+        }
     }
 }
