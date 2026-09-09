89f118963525438fd8da00b7559792477d4bcb99
kise82 <kise82@pm.me>
2026-09-09 23:42:00 +0000

    `bin/lexer_test`: Refactor file reading into a less verbose way
    Signed-off-by: kise82 <kise82@pm.me>

--- src/bin/lexer_test.rs
+++ src/bin/lexer_test.rs
@@ -1,29 +1,25 @@
 use mschief::lexer::Lexer;
 
 use std::{
-    env,
-    fs::File,
-    io::{self, BufRead, Read},
+    env, fs,
+    io::{self, BufRead},
     process,
 };
 
 fn main() {
     let file = env::args().nth(1);
-    let mut buffer = String::with_capacity(8_192);
 
     if let Some(file) = file {
-        let Ok(mut file) = File::open(file) else {
-            process::exit(1);
-        };
-        let Ok(_) = file.read_to_string(&mut buffer) else {
+        let Ok(file) = fs::read_to_string(file) else {
             process::exit(1);
         };
 
-        let lexer = Lexer::new(&buffer);
+        let lexer = Lexer::new(&file);
         for token in lexer {
             print!("{token:?} ");
         }
     } else {
+        let mut buffer = String::with_capacity(8_192);
         let mut handle = io::stdin().lock();
 
         while let Ok(n) = handle.read_line(&mut buffer)
